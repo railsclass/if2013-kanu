2d6745d5508780614f85856e92090a0034ad4d97
Travis CI <drblinken@gmail.com>
2013-09-02 12:07:06 +0200

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kanu::Application.config.secret_key_base = 'cf3b71b3cabafeb8867dc1f258a2e0a045f83cb06292ebcc635599f43c62a08e8987aa044781e1c740d83b75ce83b7d7457c16280052b5752bcc22ab50e0527c'
